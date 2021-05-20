0e998321d9f7b3a578159f97a8dd45642728074f
whoisYeshua <alien1998@yandex.ru>
2021-05-20 12:09:59 +0300

    Code optimization

---
+++ src/colorTheme.js
@@ -0,0 +1,12 @@
+const colorTheme = {
+  slugTheme: ['brightMagenta'],
+  modelTheme: ['brightCyan', 'bold'],
+  idTheme: ['bold'],
+  stockTitleTheme: ['bold'],
+  highStockTheme: ['brightGreen', 'bold'],
+  midStockTheme: ['brightYellow', 'bold'],
+  lowStockTheme: ['brightRed', 'bold'],
+  othersStockTheme: ['grey', 'bold'],
+}
+
+module.exports = { colorTheme }
---
+++ src/shemes.js
@@ -0,0 +1,30 @@
+const { countries } = require('./available-countries')
+
+const countrySchema = {
+  properties: {
+    country: {
+      description: 'Choose your country alias (index)',
+      validator: /^[A-Z]{2}$/,
+      warning: 'Use only uppercase two-letter country code (index)',
+      required: true,
+      maxLength: 2,
+      conform: value => {
+        if (value in countries) return true
+        return false
+      },
+    },
+  },
+}
+const urlSchema = {
+  properties: {
+    url: {
+      description: 'Past product URL',
+      validator: /^https:\/\/www\.nike\.com/,
+      warning:
+        'Use full product url (eg. https://www.nike.com/ru/launch/t/waffle-trainer-2-starfish)',
+      required: true,
+    },
+  },
+}
+
+module.exports = { countrySchema, urlSchema }
